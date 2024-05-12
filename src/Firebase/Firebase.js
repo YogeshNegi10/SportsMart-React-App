637cb6cb32e0bcc937fd191ab86df9265eeb6c49
Yogesh Negi <yogeshlm13@gmail.com>
2024-05-13 01:21:51 +0530

    Update Firebase.js

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 var firebaseConfig = {
-   apiKey: 'AIzaSyAWgwF2Xj6kMYnZwCuPVsWvqSZijR21M0A' ,
+   apiKey:'',
    authDomain: "sportsmart-e1ed1.firebaseapp.com",
    projectId: "sportsmart-e1ed1",
    storageBucket: "sportsmart-e1ed1.appspot.com",
