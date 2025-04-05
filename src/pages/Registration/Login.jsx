da04acf985878996ec292e334cee5c646356184d
Yogesh Negi <yogeshlm13@gmail.com>
2025-04-06 00:18:42 +0530

    Update Login.jsx

--- src/pages/Registration/Login.jsx
+++ src/pages/Registration/Login.jsx
@@ -6,64 +6,59 @@ import { auth } from "../../Firebase/Firebase";
 import { toast } from "react-toastify";
 import MyContext from "../../Context/MyContext";
 import { Circles, ColorRing } from "react-loader-spinner";
-import { FirebaseError } from 'firebase/app'
+import { FirebaseError } from 'firebase/app';
 
 const Login = () => {
   const { loading, setLoading } = useContext(MyContext);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
- 
+  
   const handleSignIn = async (e) => {
     e.preventDefault();
-     setLoading(true)
+    setLoading(true);
+    
     try {
-      const user = await signInWithEmailAndPassword(auth, email, password,);
-
+      const user = await signInWithEmailAndPassword(auth, email, password);
       localStorage.setItem("user", JSON.stringify(user));
       setLoading(false);
-      toast.success("SignIn Successfull ");
-
+      toast.success("SignIn Successfull");
       navigate("/");
     } catch (error) {
-
-  
-  if (error instanceof FirebaseError) {
-                
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorCode, errorMessage)
-                toast.error(errorMessage)
-                setLoading(false); 
-          
-             } else {
-              setLoading(false); 
-                console.log('Error:', error);
+      if (error instanceof FirebaseError) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        toast.error(errorMessage);
+      } else {
+        console.log('Error:', error);
+      }
+      setLoading(false); // Stop loading in both cases.
+    }
   };
 
-}
   return (
     <Layout>
-      <div class="  flex justify-center mt-20 mb-10 w-full">
+      <div className="flex justify-center mt-20 mb-10 w-full">
         <form onSubmit={handleSignIn}>
-          <div class="bg-white px-10 py-8 rounded-xl w-screen shadow-xl max-w-sm relative">
-            <div class="space-y-4">
-              <h1 class="text-center text-2xl font-semibold text-gray-600">
+          <div className="bg-white px-10 py-8 rounded-xl w-screen shadow-xl max-w-sm relative">
+            <div className="space-y-4">
+              <h1 className="text-center text-2xl font-semibold text-gray-600">
                 Login
               </h1>
               <hr />
-              <div class="flex items-center border-2 py-2 px-3 rounded-md mb-4">
+              <div className="flex items-center border-2 py-2 px-3 rounded-md mb-4">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5 text-gray-400"
+                  className="h-5 w-5 text-gray-400"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
                   />
                 </svg>
@@ -78,31 +73,31 @@ const Login = () => {
                 />
               </div>
 
-              <div class="flex items-center border-2 py-2 px-3 rounded-md">
+              <div className="flex items-center border-2 py-2 px-3 rounded-md">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5 text-gray-400"
+                  className="h-5 w-5 text-gray-400"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
 
                 <input
                   onChange={(e) => setPassword(e.target.value)}
-                  class="pl-2 outline-none border-none w-full"
+                  className="pl-2 outline-none border-none w-full"
                   type="password"
                   name="password"
-                  id=""
                   placeholder="Password"
                   required
                 />
               </div>
             </div>
+
             {loading ? (
               <div className="absolute top-1/3 left-40">
                 <ColorRing
@@ -121,26 +116,24 @@ const Login = () => {
                   ]}
                 />
               </div>
-            ) : (
-              ""
-            )}
+            ) : null}
 
             <button
               type="submit"
               value="login"
               id="login"
-              class="mt-6 w-full shadow-xl bg-gradient-to-tr from-blue-600 to-red-400 hover:to-red-700 text-indigo-100 py-2 rounded-md text-lg tracking-wide transition duration-1000"
+              className="mt-6 w-full shadow-xl bg-gradient-to-tr from-blue-600 to-red-400 hover:to-red-700 text-indigo-100 py-2 rounded-md text-lg tracking-wide transition duration-1000"
             >
               Login
             </button>
             <hr />
-            <div class="flex justify-center items-center mt-4">
-              <p class="inline-flex items-center text-gray-700 font-medium text-xs text-center">
-                <span class="ml-2">
+            <div className="flex justify-center items-center mt-4">
+              <p className="inline-flex items-center text-gray-700 font-medium text-xs text-center">
+                <span className="ml-2">
                   You don't have an account ?{" "}
                   <NavLink
                     to={"/register"}
-                    class="text-xs ml-2 text-blue-500 font-semibold"
+                    className="text-xs ml-2 text-blue-500 font-semibold"
                   >
                     Register now &rarr;
                   </NavLink>
@@ -151,9 +144,7 @@ const Login = () => {
         </form>
       </div>
     </Layout>
-  )
+  );
+};
 
-  };
-
-}
-export default Login
+export default Login;
